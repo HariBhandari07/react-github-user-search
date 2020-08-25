0b9b9ed9738b449ab1e2a63d6ea137ca894e9744
HariBhandari07 <bhandarihari07@gmail.com>
2020-08-25 18:41:52 +0545

    creation of user info and followers component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,61 @@
-import React, { useContext } from 'react'
-import { GithubContext } from '../context/context'
+import React, {useContext} from 'react'
+import {GithubContext} from '../context/context'
 import styled from 'styled-components'
-import { GoRepo, GoGist } from 'react-icons/go'
-import { FiUsers, FiUserPlus } from 'react-icons/fi'
+import {GoRepo, GoGist} from 'react-icons/go'
+import {FiUsers, FiUserPlus} from 'react-icons/fi'
 
 const UserInfo = () => {
-    const data = useContext(GithubContext)
-    return <h2>user info component: {data}</h2>
+    const {githubUser} = useContext(GithubContext)
+    const {public_repos, followers, following, public_gist} = githubUser
+
+    const items = [
+        {
+            id: 1,
+            icon: <GoRepo className="icon"/>,
+            label: 'repos',
+            value: public_repos,
+            color: 'pink'
+        },
+        {
+            id: 2,
+            icon: <FiUsers className="icon"/>,
+            label: 'followers',
+            value: followers,
+            color: 'green'
+        },
+        {
+            id: 3,
+            icon: <FiUserPlus className="icon"/>,
+            label: 'following',
+            value: following,
+            color: 'purple'
+        },
+        {
+            id: 1,
+            icon: <GoGist className="icon"/>,
+            label: 'gists',
+            value: public_gist,
+            color: 'yellow'
+        }
+    ]
+
+    const Item = ({icon, label, value, color}) => {
+        return <article className="item">
+            <span className={color}>{icon}</span>
+            <div>
+                <h3>{value}</h3>
+                <p>{label}</p>
+            </div>
+        </article>
+    }
+
+    return <section className="section">
+        <Wrapper className="section-center">
+            {items.map(item => {
+                return <Item key={item.id} {...item}></Item>
+            })}
+        </Wrapper>
+    </section>
 }
 
 const Wrapper = styled.section`
